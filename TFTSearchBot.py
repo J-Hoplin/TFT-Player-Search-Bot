b73b488702bbea5ab72162ed38ebce7b5f8bdc90
Hoplin <45956041+J-hoplin1@users.noreply.github.com>
2020-04-08 18:44:43 +0900

    Fix

--- TFTSearchBot.py
+++ TFTSearchBot.py
@@ -15,7 +15,7 @@ import requests
 import unicodedata
 import time
 client = discord.Client()
-token = "Njk0MTIxODAyODQ2MTc1MjQy.Xo2RJA.zicYaFogTpQHyLfBMALr-YvvtfU"
+token = ""
 
 def returnStatsTFT(bs):
     # 통계 정보
